6ede3166a5f067bc952b4d045a4b9cc0926bed50
Jack <48831311+jackmarchant1@users.noreply.github.com>
2023-05-18 11:38:01 +0100

    Color detection working

--- src/task4/src/object_detection.py
+++ src/task4/src/object_detection.py
@@ -1,72 +1,91 @@
 #!/usr/bin/env python3
 
+from pickle import TRUE
 import rospy
-from pathlib import Path 
 
 import cv2
 from cv_bridge import CvBridge, CvBridgeError 
 
 from sensor_msgs.msg import Image 
+import numpy as np
+
+from geometry_msgs.msg import Twist
 
 # Initialisations: 
 node_name = "object_detection_node"
 rospy.init_node(node_name)
 print(f"Launched the '{node_name}' node. Currently waiting for an image...")
-rate = rospy.Rate(5)
-
-base_image_path = Path.home().joinpath("myrosdata/task4_images/")
-base_image_path.mkdir(parents=True, exist_ok=True) 
+rate = rospy.Rate(2)
 
 cvbridge_interface = CvBridge() 
 
+dominant_color = None
 waiting_for_image = True 
+found_color = False
 
-def show_and_save_image(img, img_name): 
-    full_image_path = base_image_path.joinpath(f"{img_name}.jpg") 
+pub= rospy.Publisher("/cmd_vel", Twist, queue_size=10)
 
-    print("Opening the image in a new window...")
-    cv2.imshow(img_name, img) 
-    print(f"Saving the image to '{full_image_path}'...")
-    cv2.imwrite(str(full_image_path), img) 
-    print(f"Saved an image to '{full_image_path}'\n"
-        f"image dims = {img.shape[0]}x{img.shape[1]}px\n"
-        f"file size = {full_image_path.stat().st_size} bytes") 
-    print("Please close down the image pop-up window to continue...")
-    cv2.waitKey(0) 
+twist = Twist()
+twist.linear.x = 0
+twist.angular.z = 0.1
+pub.publish(twist)
 
 def camera_cb(img_data): 
-    global waiting_for_image 
+    global waiting_for_image
+    global dominant_color
+    global found_color
+
+    if found_color: return
+
+    print("camera calback")
     try:
-        cv_img = cvbridge_interface.imgmsg_to_cv2(img_data, desired_encoding="bgr8") 
+        #get image
+        cv_img = cvbridge_interface.imgmsg_to_cv2(img_data, desired_encoding="bgr8")
+        print("cv_img success") 
     except CvBridgeError as e:
         print(e)
 
-    if waiting_for_image == True: 
-        height, width, channels = cv_img.shape
+    #get hsv img
+    hsv_img = cv2.cvtColor(cv_img, cv2.COLOR_BGR2HSV)
+    colors = {
+        'red': [(0, 100, 100), (10, 255, 255)],
+        'blue': [(100, 150, 0), (140, 255, 255)],
+        'green': [(35, 100, 50), (85, 255, 255)],
+        'turquoise': [(85,200,100), (93, 250, 255)],
+        'purple': [(145,160,100), (153, 255, 255)],
+        'yellow': [(25,200,100), (40, 255, 255)]
+    }
 
-        print(f"Obtained an image of height {height}px and width {width}px.")
+    dominant_color = None
+    max_count = 0
 
-        crop_width = width - 400
-        crop_height = 400
-        crop_y0 = int((width / 2) - (crop_width / 2))
-        crop_z0 = int((height / 2) - (crop_height / 2))
-        cropped_img = cv_img[crop_z0:crop_z0+crop_height, crop_y0:crop_y0+crop_width]
+    for color, (lower, upper) in colors.items():
+        lower = np.array(lower, dtype=np.uint8)
+        upper = np.array(upper, dtype=np.uint8)
+        mask = cv2.inRange(hsv_img, lower, upper)
+        count = cv2.countNonZero(mask)
+        print(count)
+        total_pixel_count = hsv_img.shape[0] * hsv_img.shape[1]
+        #if the amount of a color of pixel is dominant in the image, it is in that zone
+        if count > total_pixel_count/2:
+            print("resetting dominant color")
+            max_count = count
+            dominant_color = color
 
-        show_and_save_image(cropped_img, img_name = "step2_cropping")
-        hsv_img = cv2.cvtColor(cropped_img, cv2.COLOR_BGR2HSV)
-        lower_threshold = (115, 225, 100)
-        upper_threshold = (130, 255, 255)
-        img_mask = cv2.inRange(hsv_img, lower_threshold, upper_threshold)
-
-        show_and_save_image(img_mask, img_name = "step3_image_mask")
-
-
-
-        waiting_for_image = False
+    if dominant_color:
+        print(f"The dominant color in the initial zone is {dominant_color}")
+        found_color = True
+    
 
 rospy.Subscriber("/camera/rgb/image_raw", Image, camera_cb) 
 
-while waiting_for_image: 
+while not found_color:
+    pub.publish(twist)
     rate.sleep()
+
+
+twist.angular.z = 0
+pub.publish(twist)
+
 
 cv2.destroyAllWindows() 
