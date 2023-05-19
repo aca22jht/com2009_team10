d16cdec9ba28adda0d40573f53196d33367fb123
Zheng626 <128692801+Zheng626@users.noreply.github.com>
2023-05-20 04:48:12 +0800

    Update task3.py

--- src/task3.py
+++ src/task3.py
@@ -7,59 +7,58 @@ from sensor_msgs.msg import LaserScan
 import numpy as np
 
 
-class Task3Maze:
+class task3:
 
     def __init__(self):
         self.node_name = "task3"
 
-        self.pub_velocity = rospy.Publisher('cmd_vel', Twist, queue_size=10)
-        self.sub_lidar = rospy.Subscriber('scan', LaserScan, self.scan_callback)
-        #iniate the node
+        self.velocity_publisher = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+        self.lidar_subscriber = rospy.Subscriber('scan', LaserScan, self.scan_callback)
+        # Initialize the node
         rospy.init_node(self.node_name, anonymous=True)
         self.rate = rospy.Rate(15)  
 
-        self.min_distance = 1
+        self.min_distance_threshold = 0.45
         self.front_arc = np.empty(180)
-        self.cmd = Twist()
+        self.robot_command = Twist()
 
         self.ctrl_c = False
-        rospy.on_shutdown(self.shutdownhook)
+        rospy.on_shutdown(self.shutdown_hook)
 
-        #rospy.loginfo(f"the {self.node_name} node has been initialised...")
-
-    def shutdownhook(self):
-        self.pub_velocity.publish(Twist())
+    def shutdown_hook(self):
+        self.velocity_publisher.publish(Twist())
         self.ctrl_c = True
 
-    def main_loop(self):
-
+    def solve_maze(self):
+        """
+        Main loop for the maze-solving algorithm.
+        """
         while not self.ctrl_c:
+            front_distance = np.amin(self.front_arc[75:105])
+            right_side_distance = np.amin(self.front_arc[120:150])
 
-            front = np.amin(self.front_arc[75:105])
-            right_Side = np.amin(self.front_arc[120:150])
-
-            if front > 0.45:
-                if right_Side < 0.3:
-                    print("turn left")
-                    self.cmd.angular.z = 0.9 
-                    self.cmd.linear.x = 0.26 
-                elif 0.3 < right_Side < 0.4:
-                    print("go straight")
-                    self.cmd.angular.z = 0 
-                    self.cmd.linear.x = 0.26 
+            if front_distance > self.min_distance_threshold:
+                if right_side_distance < 0.3:
+                    print("Turning left")
+                    self.robot_command.angular.z = 0.9 
+                    self.robot_command.linear.x = 0.26 
+                elif 0.3 < right_side_distance < 0.4:
+                    print("Going straight")
+                    self.robot_command.angular.z = 0 
+                    self.robot_command.linear.x = 0.26 
                 else:
-                    print("turn right")
-                    self.cmd.angular.z = -0.9
-                    self.cmd.linear.x = 0.26 
+                    print("Turning right")
+                    self.robot_command.angular.z = -0.9
+                    self.robot_command.linear.x = 0.26 
             else:
                 print("Front obstacle detected. Turning away.")
-                self.cmd.angular.z = 0 
-                self.cmd.linear.x = 0 
-                self.pub_velocity.publish(self.cmd)
-                self.cmd.angular.z = 1.2
-                self.pub_velocity.publish(self.cmd)
+                self.robot_command.angular.z = 0 
+                self.robot_command.linear.x = 0 
+                self.velocity_publisher.publish(self.robot_command)
+                self.robot_command.angular.z = 1.2
+                self.velocity_publisher.publish(self.robot_command)
 
-            self.pub_velocity.publish(self.cmd)
+            self.velocity_publisher.publish(self.robot_command)
             self.rate.sleep()
 
     def scan_callback(self, scan_data):
@@ -71,9 +70,13 @@ class Task3Maze:
         arc_angles = np.arange(-90, 91)
         self.object_angle = arc_angles[np.argmin(self.front_arc)]
 
+
 if __name__ == '__main__':
-    task3Maze_instance = Task3Maze()
+    maze_solver_instance = task3()
     try:
-        task3Maze_instance.main_loop()
+        maze_solver_instance.solve_maze()
+    except rospy.ROSInterruptException:
+        pass
+
     except rospy.ROSInterruptException:
         pass    
